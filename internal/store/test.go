18816146d77da82efcde0f1fc661eb1603f4375f
bmaxammatov@webster.edu <153611369+makhammatovb@users.noreply.github.com>
2025-10-31 21:06:54 +0500

    test

--- internal/store/test.go
+++ internal/store/test.go
@@ -18,7 +18,7 @@ func setupTestDB(t *testing.T) *sql.DB {
 	if err != nil {
 		t.Fatalf("Failed to run migrations: %v", err)
 	}
-	_, err = db.Exec("TRUNCATE TABLE articles, paragraphs CASCADE")
+	_, err = db.Exec("TRUNCATE TABLE users, articles, paragraphs CASCADE")
 	if err != nil {
 		t.Fatalf("Failed to truncate tables: %v", err)
 	}
@@ -114,7 +114,55 @@ func TestCreateArticle(t *testing.T) {
 				assert.Equal(t, tt.article.Paraghraps[i].OrderIndex, paragraph.OrderIndex)
 				assert.NotZero(t, paragraph.ID)
 			}
-			
+		})
+	}
+}
+
+func TestCreateUser(t *testing.T) {
+	db := setupTestDB(t)
+	defer db.Close()
+	
+	store := NewPostgresUserStore(db)
+	
+	tests := []struct {
+		name    string
+		user    *User
+		wantErr bool
+	}{
+		{
+			name: "Valid User",
+			user: &User{
+				Email:        "c2KQw@example.com",
+				PasswordHash: "hashed_password",
+				FirstName:    "John",
+				LastName:     "Doe",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid User",
+			user: &User{
+				Email:        "invalid_email",
+				PasswordHash: "hashed_password",
+				FirstName:    "John",
+				LastName:     "Doe",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			createdUser, err := store.CreateUser(tt.user)
+			if tt.wantErr {
+				assert.Error(t, err, "Expected error but got none")
+				return
+			}
+			require.NoError(t, err)
+			assert.Equal(t, tt.user.Email, createdUser.Email)
+			assert.Equal(t, tt.user.PasswordHash, createdUser.PasswordHash)
+			assert.Equal(t, tt.user.FirstName, createdUser.FirstName)
+			assert.Equal(t, tt.user.LastName, createdUser.LastName)
+			assert.NotZero(t, createdUser.ID)
 		})
 	}
 }
